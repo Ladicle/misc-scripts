eb90083ac4047c62d1ae146f7febacc131a17e4c
Aya Igarashi <Ladiclexxx@gmail.com>
2017-10-31 16:28:27 +0900

    Create check_fish_color.fish

---
+++ check_fish_color.fish
@@ -0,0 +1,12 @@
+#!/usr/local/bin/fish
+
+# ref
+# http://fish.rubikitch.com/document-ja/
+
+echo (set_color $fish_color_normal)fish_color_normal
+echo (set_color $fish_color_command)fish_color_command
+echo (set_color $fish_color_quote)fish_color_quote
+echo (set_color $fish_color_redirection)fish_color_redirection
+echo (set_color $fish_color_end)fish_color_end
+echo (set_color $fish_color_error)fish_color_error
+echo (set_color $fish_color_param)fish_color_param
